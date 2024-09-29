ccb7bb4c617c83dcc340a8e6c2ee49face0880d6
Igorsampa <igor.silva@sou.inteli.br.edu>
2024-09-29 13:37:48 -0300

    feat:resposta

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,58 +3,28 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
-# 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2)).apply(lambda x: 1 if x > 25 else 0)
 
-# 3
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 
-
-# 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
-    
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    # Criação de gráfico
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
+    fig = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar')
     fig.savefig('catplot.png')
     return fig
 
-
-# 10
 def draw_heat_map():
-    # 11
-    df_heat = None
-
-    # 12
-    corr = None
-
-    # 13
-    mask = None
-
-
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
+    # Criação de gráfico
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) &(df['weight'] <= df['weight'].quantile(0.975))]
+    corr = df_heat.corr().round(1)
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+    fig, ax = plt.subplots(figsize=(10, 8))
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', cmap='hot', linewidths=0.5, ax=ax)
     fig.savefig('heatmap.png')
     return fig
+    
